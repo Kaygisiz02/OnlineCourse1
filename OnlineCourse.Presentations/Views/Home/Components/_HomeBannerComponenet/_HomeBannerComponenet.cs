1cdeea3d242e2b89ab0c087a993ba17ccf853809
Kaygisiz02 <bektasbaykara@gmail.com>
2024-12-20 22:56:34 +0300

    Bannercomponentin homeIndexe taşınası

---
+++ OnlineCourse.Presentations/Views/Home/Components/_HomeBannerComponenet/_HomeBannerComponenet.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineCourse.Presentations.Views.Home.Components._HomeBannerComponenet
+{
+    public class _HomeBannerComponenet : ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var response = await _client.GetFromJsonAsync<List<BannerDto>>("banner");
+            return View(response);
+        }
+    }
+}
