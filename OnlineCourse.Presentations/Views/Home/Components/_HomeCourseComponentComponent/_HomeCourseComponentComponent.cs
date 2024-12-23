5fb0df1be3201f16f07ba19e9f61e91a5d600a8f
Kaygisiz02 <bektasbaykara@gmail.com>
2024-12-23 15:20:50 +0300

    IsShow gösterimi ve düzenlenmesi

---
+++ OnlineCourse.Presentations/Views/Home/Components/_HomeCourseComponentComponent/_HomeCourseComponentComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineCourse.Presentations.Views.Home.Components._HomeCourseComponentComponent
+{
+    public class _HomeCourseComponentComponent:ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var response = await _client.GetFromJsonAsync<List<CourseDto>>("course/GetActiveCourses");
+            return View(response);
+        }
+    }
+}
